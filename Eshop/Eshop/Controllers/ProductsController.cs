8cddc4f91ffe0dd2a51e393750db9a27d9984428
Minh08122002 <0306201551@caothang.edu.vn>
2022-10-19 15:30:44 +0700

    Hello

---
+++ Eshop/Eshop/Controllers/ProductsController.cs
@@ -0,0 +1,21 @@
+﻿using Eshop.Data;
+using Eshop.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Eshop.Controllers
+{
+    public class ProductsController : Controller
+    {
+        private EshopContext _context;
+
+        public ProductsController(EshopContext context)
+        {
+            _context = context;
+        }
+        public IActionResult Index()
+        {
+            var products = _context.Products.ToList();
+            return View(products);
+        }
+    }
+}
